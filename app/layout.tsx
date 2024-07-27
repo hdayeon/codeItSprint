8d7a7884396b74fc22d1ea11b1e94b366369dc80
dy <gkekdus97@naver.com>
2024-07-28 02:40:43 +0900

    style: 폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "../public/fonts/style.css";
 import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
